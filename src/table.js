684b05bea8a2c1210fbc78203eecd93fb0491421
DexTher22 <markmagyar2003@gmail.com>
2026-03-19 13:06:50 +0100

    na

---
+++ src/table.js
@@ -0,0 +1,7 @@
+employees = [
+    { id: 1, name: 'Erős István', city: 'Szeged', salary: '395'}
+    { id: 2, name: 'Tar Ferenc', city: 'Pécs', salary: '392'}
+    { id: 3, name: 'Csaló Gábor', city: 'Szeged', salary: '398'}
+    { id: 4, name: 'Rendes Irén', city: 'Pécs', salary: '393'}
+    { id: 5, name: 'Fel Áron', city: 'Szeged', salary: '397'}
+]
